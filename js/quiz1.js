4b9611ac1ccfe1f697897496da958c04c69c4923
AnuKPto <37680296+Nagato41@users.noreply.github.com>
2020-09-16 13:51:16 +0700

    Update quiz1.js

--- js/quiz1.js
+++ js/quiz1.js
@@ -1,11 +1,12 @@
 let person = {
-    name : 'Anukoon Prato',
+    fname : 'Anukoon',
+    lname : 'Prato',
     age : 22,
     nick_name: 'Lek',
     free_time: 'Games',
     phone: '0612593296'
 }
 
-console.log("Name",person.name,"Age",person.age,"Nick Name",
+console.log("Name",person.fname,person.lname,"Age",person.age,"Nick Name",
 person.nick_name,"Free Time",person.free_time,"Phone",person.phone
 )
