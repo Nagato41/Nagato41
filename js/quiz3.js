8392fc02e4110f545ba81d7ca0730309a85e22f5
AnuKPto <37680296+Nagato41@users.noreply.github.com>
2020-09-16 15:20:10 +0700

    Create quiz3.js

---
+++ js/quiz3.js
@@ -0,0 +1,22 @@
+let point = 50
+
+if( point <= 100 && point >= 81){
+    console.log('A')
+
+}
+if( point <= 80 && point >= 71){
+    console.log('B')
+
+}
+if( point <= 70 && point >= 61){
+    console.log('A')
+
+}
+if( point <= 60 && point >= 51){
+    console.log('B')
+
+}
+else{
+    console.log('F')
+
+}
