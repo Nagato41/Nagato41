26ddea4d0782d826060d982f0f707203163473f9
AnuKPto <37680296+Nagato41@users.noreply.github.com>
2020-09-16 13:38:04 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+
+const PI = 3.14
+
+console.log("PI" ,PI)
+
+
+let arr = ['zpple','banana','kiwi']
+console.log("arr " , arr[1])
+
+let person = {
+    name : 'Anukoon',
+    age : '16'
+}
+
+console.log("Person",person)
